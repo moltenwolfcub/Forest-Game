3a25c981194b961bdad445cf5c3f7f44b4af304f
MoltenWolfCub <oliver@tomlins.org.uk>
2023-04-11 18:54:59 +0100

    extracted game struct to seperate package

--- main.go
+++ main.go
@@ -3,28 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/moltenwolfcub/Forest-Game/game"
 )
 
-type Game struct{}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
-}
-
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	gameInstance := game.Game{}
+	if err := gameInstance.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
