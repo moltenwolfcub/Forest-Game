f5c34273fd392e137cc71c723638ba22e52338e7
MoltenWolfCub <oliver@tomlins.org.uk>
2023-06-17 15:29:42 +0100

    fixed colour of the river
    was green now blue

--- game/river.go
+++ game/river.go
@@ -17,7 +17,7 @@ func (r River) Hitbox(GameContext) image.Rectangle {
 
 func (r River) DrawAt(screen *ebiten.Image, pos image.Point) {
 	img := ebiten.NewImage(r.Collision.Dx(), r.Collision.Dy())
-	img.Fill(color.RGBA{50, 140, 0, 255})
+	img.Fill(color.RGBA{72, 122, 173, 255})
 
 	options := ebiten.DrawImageOptions{}
 	options.GeoM.Translate(float64(pos.X), float64(pos.Y))
