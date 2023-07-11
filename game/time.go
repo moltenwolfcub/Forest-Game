ef58dd342615e781532561cec90cd6ff9d3d07d1
MoltenWolfCub <oliver@tomlins.org.uk>
2023-07-11 20:56:18 +0100

    implemented basic seasons
    time display now shows the current season

--- game/time.go
+++ game/time.go
@@ -3,6 +3,9 @@ package game
 import (
 	"fmt"
 	"math"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 //  Minute
@@ -40,6 +43,49 @@ const (
 	MonthsPerYear = 8
 )
 
+type Season int
+
+const (
+	Spring Season = iota
+	Summer
+	Autumn
+	Winter
+)
+
+// Returns a Season from a month in the year.
+// The parsed month should be 1-indexed so 0 in invalid.
+//
+// Any value provided outside of the range 1-8 will panic
+func GetSeason(monthInYear int) Season {
+	switch monthInYear {
+	case 1, 2:
+		return Spring
+	case 3, 4:
+		return Summer
+	case 5, 6:
+		return Autumn
+	case 7, 8:
+		return Winter
+	default:
+		panic(fmt.Sprintf("Can't figure out season from month: %d. Season.GetSeason only accepts values in the range 1-8 inclusive.", monthInYear))
+	}
+}
+
+func (s Season) String() string {
+	switch s {
+	case Spring:
+		return "spring"
+	case Summer:
+		return "summer"
+	case Autumn:
+		return "autumn"
+	case Winter:
+		return "winter"
+	default:
+		return "error"
+	}
+}
+
 type Time int
 
 func (t *Time) Tick() {
@@ -60,7 +106,9 @@ func (t Time) String() string {
 	months := totalMonths%MonthsPerYear + 1
 	years := totalYears
 
-	return fmt.Sprintf("Season[unimplemented] %d/%d/%d %02d:%02d", days, months, years, hours, mins)
+	season := cases.Title(language.AmericanEnglish).String(GetSeason(int(months)).String())
+
+	return fmt.Sprintf("%s %d/%d/%d %02d:%02d", season, days, months, years, hours, mins)
 }
 
 // Returns the number of minutes through the day it currently is
