f5c601037ee3dae321725846cf0f1b446e14d7ac
MoltenWolfCub <oliver@tomlins.org.uk>
2023-07-15 10:54:30 +0100

    removed exampleFlag
    When making the cmd flags handler the exampleFlag was left there
    and it does nothing so it shouldn't be there anymore. Now removed.

--- args/flags.go
+++ args/flags.go
@@ -3,12 +3,10 @@ package args
 import "flag"
 
 var (
-	exampleFlag  int
 	TimeRateFlag int
 )
 
 func ParseFlags() {
-	flag.IntVar(&exampleFlag, "example", 5, "example flag")
 	flag.IntVar(&TimeRateFlag, "time-rate", 1, "A multiplier for the rate that time should progress. 1 is normal time rate")
 
 	flag.Parse()
