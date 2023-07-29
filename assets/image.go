b5a05028406144b8f0ebce0a7e3c7e81e634a410
MoltenWolfCub <oliver@tomlins.org.uk>
2023-07-29 10:09:40 +0100

    refactored image embedding
    rather than embedding every image as a separate byte slice, it uses an
    `embed.FS` to store all the textures together and then extract and
    convert them into images in `image.go`

--- assets/image.go
+++ assets/image.go
@@ -8,7 +8,13 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-func LoadPNG(embeddedImage []byte) *ebiten.Image {
+func LoadPNG(file string) *ebiten.Image {
+
+	embeddedImage, err := textures.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+
 	imageDecoded, _, err := image.Decode(bytes.NewReader(embeddedImage))
 	if err != nil {
 		panic(err)
@@ -17,43 +23,43 @@ func LoadPNG(embeddedImage []byte) *ebiten.Image {
 }
 
 var (
-	Player *ebiten.Image = LoadPNG(playerBytes)
-	Tree   *ebiten.Image = LoadPNG(treeBytes)
+	Player *ebiten.Image = LoadPNG("player.png")
+	Tree   *ebiten.Image = LoadPNG("tree.png")
 
-	Icon16  *ebiten.Image = LoadPNG(icon16Bytes)
-	Icon22  *ebiten.Image = LoadPNG(icon22Bytes)
-	Icon24  *ebiten.Image = LoadPNG(icon24Bytes)
-	Icon32  *ebiten.Image = LoadPNG(icon32Bytes)
-	Icon48  *ebiten.Image = LoadPNG(icon48Bytes)
-	Icon64  *ebiten.Image = LoadPNG(icon64Bytes)
-	Icon128 *ebiten.Image = LoadPNG(icon128Bytes)
-	Icon256 *ebiten.Image = LoadPNG(icon256Bytes)
-	Icon512 *ebiten.Image = LoadPNG(icon512Bytes)
+	Icon16  *ebiten.Image = LoadPNG("icon/icon16.png")
+	Icon22  *ebiten.Image = LoadPNG("icon/icon22.png")
+	Icon24  *ebiten.Image = LoadPNG("icon/icon24.png")
+	Icon32  *ebiten.Image = LoadPNG("icon/icon32.png")
+	Icon48  *ebiten.Image = LoadPNG("icon/icon48.png")
+	Icon64  *ebiten.Image = LoadPNG("icon/icon64.png")
+	Icon128 *ebiten.Image = LoadPNG("icon/icon128.png")
+	Icon256 *ebiten.Image = LoadPNG("icon/icon256.png")
+	Icon512 *ebiten.Image = LoadPNG("icon/icon512.png")
 
-	BerriesLight1 *ebiten.Image = LoadPNG(lightBerries1Bytes)
-	BerriesLight2 *ebiten.Image = LoadPNG(lightBerries2Bytes)
-	BerriesLight3 *ebiten.Image = LoadPNG(lightBerries3Bytes)
-	BerriesLight4 *ebiten.Image = LoadPNG(lightBerries4Bytes)
-	BerriesLight5 *ebiten.Image = LoadPNG(lightBerries5Bytes)
-	BerriesLight6 *ebiten.Image = LoadPNG(lightBerries6Bytes)
-	BerriesLight7 *ebiten.Image = LoadPNG(lightBerries7Bytes)
-	BerriesLight8 *ebiten.Image = LoadPNG(lightBerries8Bytes)
+	BerriesLight1 *ebiten.Image = LoadPNG("berries/light1.png")
+	BerriesLight2 *ebiten.Image = LoadPNG("berries/light2.png")
+	BerriesLight3 *ebiten.Image = LoadPNG("berries/light3.png")
+	BerriesLight4 *ebiten.Image = LoadPNG("berries/light4.png")
+	BerriesLight5 *ebiten.Image = LoadPNG("berries/light5.png")
+	BerriesLight6 *ebiten.Image = LoadPNG("berries/light6.png")
+	BerriesLight7 *ebiten.Image = LoadPNG("berries/light7.png")
+	BerriesLight8 *ebiten.Image = LoadPNG("berries/light8.png")
 
-	BerriesMid1 *ebiten.Image = LoadPNG(midBerries1Bytes)
-	BerriesMid2 *ebiten.Image = LoadPNG(midBerries2Bytes)
-	BerriesMid3 *ebiten.Image = LoadPNG(midBerries3Bytes)
-	BerriesMid4 *ebiten.Image = LoadPNG(midBerries4Bytes)
-	BerriesMid5 *ebiten.Image = LoadPNG(midBerries5Bytes)
-	BerriesMid6 *ebiten.Image = LoadPNG(midBerries6Bytes)
-	BerriesMid7 *ebiten.Image = LoadPNG(midBerries7Bytes)
-	BerriesMid8 *ebiten.Image = LoadPNG(midBerries8Bytes)
+	BerriesMid1 *ebiten.Image = LoadPNG("berries/mid1.png")
+	BerriesMid2 *ebiten.Image = LoadPNG("berries/mid2.png")
+	BerriesMid3 *ebiten.Image = LoadPNG("berries/mid3.png")
+	BerriesMid4 *ebiten.Image = LoadPNG("berries/mid4.png")
+	BerriesMid5 *ebiten.Image = LoadPNG("berries/mid5.png")
+	BerriesMid6 *ebiten.Image = LoadPNG("berries/mid6.png")
+	BerriesMid7 *ebiten.Image = LoadPNG("berries/mid7.png")
+	BerriesMid8 *ebiten.Image = LoadPNG("berries/mid8.png")
 
-	BerriesDark1 *ebiten.Image = LoadPNG(darkBerries1Bytes)
-	BerriesDark2 *ebiten.Image = LoadPNG(darkBerries2Bytes)
-	BerriesDark3 *ebiten.Image = LoadPNG(darkBerries3Bytes)
-	BerriesDark4 *ebiten.Image = LoadPNG(darkBerries4Bytes)
-	BerriesDark5 *ebiten.Image = LoadPNG(darkBerries5Bytes)
-	BerriesDark6 *ebiten.Image = LoadPNG(darkBerries6Bytes)
-	BerriesDark7 *ebiten.Image = LoadPNG(darkBerries7Bytes)
-	BerriesDark8 *ebiten.Image = LoadPNG(darkBerries8Bytes)
+	BerriesDark1 *ebiten.Image = LoadPNG("berries/dark1.png")
+	BerriesDark2 *ebiten.Image = LoadPNG("berries/dark2.png")
+	BerriesDark3 *ebiten.Image = LoadPNG("berries/dark3.png")
+	BerriesDark4 *ebiten.Image = LoadPNG("berries/dark4.png")
+	BerriesDark5 *ebiten.Image = LoadPNG("berries/dark5.png")
+	BerriesDark6 *ebiten.Image = LoadPNG("berries/dark6.png")
+	BerriesDark7 *ebiten.Image = LoadPNG("berries/dark7.png")
+	BerriesDark8 *ebiten.Image = LoadPNG("berries/dark8.png")
 )
