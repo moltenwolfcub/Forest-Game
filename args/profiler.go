c3fb1563e650b89fb4cabe6e3fcd764d82a3349e
MoltenWolfCub <oliver@tomlins.org.uk>
2023-10-07 21:16:42 +0100

    added time to log file names
    now log files are named with the time they were ran. A latest file was
    also implemented for qol

--- args/profiler.go
+++ args/profiler.go
@@ -1,29 +1,32 @@
 package args
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"log/slog"
 	"os"
 	"runtime/pprof"
 	"time"
 )
 
+const logDir = "profilerLogs"
+
 func Profile() func() {
-	fileCpu, err := os.Create("profilerLogs/cpu.pprof")
-	if err != nil {
-		slog.Error("Error creating file for cpu profiler: " + err.Error())
+	if _, err := os.Stat(logDir); errors.Is(err, os.ErrNotExist) {
+		if err := os.Mkdir(logDir, os.ModePerm); err != nil {
+			slog.Error(fmt.Sprintf("Error creating logger directory: %v", err.Error()))
+		}
 	}
+
+	fileNameDate := time.Now().Format("2006-01-02-15:04:05")
+
+	fileCpu, fileLatestCpu := createLog(fileNameDate, "cpu", false)
+	fileMem, fileLatestMem := createLog(fileNameDate, "memory", false)
+	fileInfo, fileLatestInfo := createLog(fileNameDate, "info", true)
+
 	pprof.StartCPUProfile(fileCpu)
-
-	fileMem, err := os.Create("profilerLogs/memory.pprof")
-	if err != nil {
-		slog.Error("Error creating file for memory profiler: " + err.Error())
-	}
 	pprof.WriteHeapProfile(fileMem)
-
-	fileInfo, err := os.Create("profilerLogs/info.log")
-	if err != nil {
-		slog.Error("Error creating file for info of profiler: " + err.Error())
-	}
 	startTime := time.Now()
 
 	return func() {
@@ -34,5 +37,52 @@ func Profile() func() {
 		fileInfo.WriteString("Start Time: " + startTime.String() + "\n")
 		fileInfo.WriteString("End Time: " + time.Now().String() + "\n")
 		fileInfo.Close()
+
+		writeLatest(fileLatestCpu, "cpu", fileNameDate, false)
+		writeLatest(fileLatestMem, "memory", fileNameDate, false)
+		writeLatest(fileLatestInfo, "info", fileNameDate, true)
 	}
+}
+
+func writeLatest(latestFile *os.File, profileType string, fileNameDate string, logExt bool) {
+	var ext string
+	if logExt {
+		ext = "log"
+	} else {
+		ext = "pprof"
+	}
+
+	file, err := os.Open(fmt.Sprintf("%s/%s-%s.%s", logDir, fileNameDate, profileType, ext))
+	fileError(profileType, err)
+
+	_, err = io.Copy(latestFile, file)
+	if err != nil {
+		slog.Error(fmt.Sprintf("Error writing to latest %s profiler file: %v", profileType, err.Error()))
+	}
+
+	file.Close()
+}
+
+func fileError(profileType string, err error) {
+	if err != nil {
+		slog.Error(fmt.Sprintf("Error creating file for %s profiler: %v", profileType, err.Error()))
+	}
+}
+
+func createLog(time string, profileType string, logExt bool) (log, latest *os.File) {
+	var err error
+
+	var ext string
+	if logExt {
+		ext = "log"
+	} else {
+		ext = "pprof"
+	}
+
+	log, err = os.Create(fmt.Sprintf("%s/%s-%s.%s", logDir, time, profileType, ext))
+	fileError(profileType, err)
+	latest, err = os.Create(fmt.Sprintf("%s/latest-%s.%s", logDir, profileType, ext))
+	fileError(profileType, err)
+
+	return
 }
