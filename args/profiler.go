b243ffbad03074b8ac486ee8c48fc3908746913c
MoltenWolfCub <oliver@tomlins.org.uk>
2023-10-07 20:09:03 +0100

    added an info to profiler logs
    stores runtime and start and end times of the profiled run

--- args/profiler.go
+++ args/profiler.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 	"os"
 	"runtime/pprof"
+	"time"
 )
 
 func Profile() func() {
@@ -19,8 +20,19 @@ func Profile() func() {
 	}
 	pprof.WriteHeapProfile(fileMem)
 
+	fileInfo, err := os.Create("profilerLogs/info.log")
+	if err != nil {
+		slog.Error("Error creating file for info of profiler: " + err.Error())
+	}
+	startTime := time.Now()
+
 	return func() {
 		pprof.StopCPUProfile()
 		fileMem.Close()
+
+		fileInfo.WriteString("Run Duration: " + time.Since(startTime).String() + "\n")
+		fileInfo.WriteString("Start Time: " + startTime.String() + "\n")
+		fileInfo.WriteString("End Time: " + time.Now().String() + "\n")
+		fileInfo.Close()
 	}
 }
