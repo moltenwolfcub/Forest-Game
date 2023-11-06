dd2e3a2944c994c2c48bfab47297515a9f1c689e
MoltenWolfCub <oliver@tomlins.org.uk>
2023-11-06 20:30:05 +0000

    scaled down inventory
    inventory image is now scaled to actually fit in the screen rather than
    going off the edge

--- game/inventory.go
+++ game/inventory.go
@@ -48,11 +48,25 @@ func (i InventoryOverlay) Origin(layer GameContext) (image.Point, error) {
 }
 
 func (i InventoryOverlay) Size(_ GameContext) (image.Point, error) {
-	return assets.Inventory.Bounds().Size(), nil
+	return image.Pt(960, 960), nil
 }
 
-func (i InventoryOverlay) GetHitbox(_ GameContext) ([]image.Rectangle, error) {
-	return []image.Rectangle{assets.Inventory.Bounds()}, nil
+func (i InventoryOverlay) GetHitbox(layer GameContext) ([]image.Rectangle, error) {
+	origin, err := i.Origin(layer)
+	if err != nil {
+		return nil, err
+	}
+	size, err := i.Size(layer)
+	if err != nil {
+		return nil, err
+	}
+
+	r := image.Rectangle{
+		Min: origin,
+		Max: origin.Add(size),
+	}
+
+	return []image.Rectangle{r}, nil
 }
 
 func (i InventoryOverlay) DrawAt(screen *ebiten.Image, pos image.Point) error {
@@ -61,6 +75,11 @@ func (i InventoryOverlay) DrawAt(screen *ebiten.Image, pos image.Point) error {
 	}
 
 	options := ebiten.DrawImageOptions{}
+
+	targetSize, _ := i.Size(Render)
+	sf := float64(targetSize.Y) / float64(assets.Inventory.Bounds().Size().Y)
+	options.GeoM.Scale(float64(sf), float64(sf))
+
 	options.GeoM.Translate(float64(pos.X), float64(pos.Y))
 
 	screen.DrawImage(assets.Inventory, &options)
