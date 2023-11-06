aca83b3955125f3f4f6d4f87a6668c15b26bb4ca
MoltenWolfCub <oliver@tomlins.org.uk>
2023-11-06 21:08:19 +0000

    properly removed the corners
    pulled the lineart back a full lineart width so that there is enough
    space for the curves

--- game/lineart.go
+++ game/lineart.go
@@ -167,11 +167,11 @@ func generateLineSegment(start image.Point, end image.Point, isHorizontal bool) 
 	var lineSeg *ebiten.Image
 	lineSegOps := ebiten.DrawImageOptions{}
 	if isHorizontal {
-		lineSeg = ebiten.NewImage(end.X-start.X, lineartW)
-		lineSegOps.GeoM.Translate(0, -float64(lineartW/2))
+		lineSeg = ebiten.NewImage(end.X-start.X-lineartW, lineartW)
+		lineSegOps.GeoM.Translate(float64(lineartW)/2, -float64(lineartW)/2)
 	} else {
-		lineSeg = ebiten.NewImage(lineartW, end.Y-start.Y)
-		lineSegOps.GeoM.Translate(-float64(lineartW/2), 0)
+		lineSeg = ebiten.NewImage(lineartW, end.Y-start.Y-lineartW)
+		lineSegOps.GeoM.Translate(-float64(lineartW)/2, float64(lineartW)/2)
 	}
 	lineSeg.Fill(LineartColor)
 
